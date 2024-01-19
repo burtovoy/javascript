81b3d754bc7e613324515a8bdf8fbd3e9e3c6fc9
Glukozavrik <ploplotroll@gmail.com>
2024-01-19 10:17:13 +0300

    space_age

--- space_age/space-age.js
+++ space_age/space-age.js
@@ -1,23 +1,22 @@
 export const age = (planet, seconds) => {
   let planetAge = 0;
-  if (planet.toLowerCase() === 'earth') {
+  planet = planet.toLowerCase()
+  if (planet === 'earth') {
     planetAge = seconds / 31557600;
-  } else if (planet.toLowerCase() === 'mercury') {
+  } else if (planet === 'mercury') {
     planetAge = seconds / (31557600 * 0.2408467);
-  } else if (planet.toLowerCase() === 'venus') {
+  } else if (planet === 'venus') {
     planetAge = seconds / (31557600 * 0.61519726);
-  } else if (planet.toLowerCase() === 'mars') {
+  } else if (planet === 'mars') {
     planetAge = seconds / (31557600 * 1.8808158);
-  } else if (planet.toLowerCase() === 'jupiter') {
+  } else if (planet === 'jupiter') {
     planetAge = seconds / (31557600 * 11.862615);
-  } else if (planet.toLowerCase() === 'saturn') {
+  } else if (planet === 'saturn') {
     planetAge = seconds / (31557600 * 29.447498);
-  } else if (planet.toLowerCase() === 'uranus') {
+  } else if (planet === 'uranus') {
     planetAge = seconds / (31557600 * 84.016846);
-  } else if (planet.toLowerCase() === 'neptune') {
+  } else if (planet === 'neptune') {
     planetAge = seconds / (31557600 * 164.79132);
-  } else {
-    console.log('try again');
   }
   planetAge = Number(planetAge.toFixed(2));
   return planetAge;
