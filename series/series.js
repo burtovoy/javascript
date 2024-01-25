485217d796821035aa863fbfdc5127ee2c0258f7
Glukozavrik <ploplotroll@gmail.com>
2024-01-25 13:49:44 +0300

    -upd

--- series/series.js
+++ series/series.js
@@ -1,14 +1,28 @@
-//
-// This is only a SKELETON file for the 'Series' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Series {
   constructor(series) {
-    throw new Error('Remove this statement and implement this function');
+    if (series = 0) {
+      throw new Error("series cannot be empty")
+    } else {
+      this.series = series
+    }
   }
 
   slices(sliceLength) {
-    throw new Error('Remove this statement and implement this function');
+    if (sliceLength = 0) {
+      throw new Error("slice length cannot be zero")
+    } else if (sliceLength < 0) {
+      throw new Error("slice length cannot be negative")
+    }else if (sliceLength > 0){
+      let newArr = this.series
+      newArr.split("")
+      for (i = 0, i < newArr.length, i += 1) {
+      if ((newArr[i] + sliceLength) <= newArr.length) {
+        newArr.slice(newArr[i], (newArr[i] + sliceLength))
+       } else {
+        throw new Error("slice length cannot be greater than series length")
+       }
+       return newArr
+     }
+  }
   }
 }
