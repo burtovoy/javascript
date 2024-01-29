4dc2ddf7b0403b527b8763f540bb870289df0db0
Elena <110896958+Elena-web@users.noreply.github.com>
2024-01-29 20:39:56 +0300

    task Space-age

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -4,7 +4,21 @@
 //
 
 
-export const age = () => {
-  
-  return;
-};
+export function age (planet, sec) {  
+  let yearPlanet = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  };
+  const earth1 = 31557600;
+  let res = sec / (yearPlanet[planet] * earth1);
+  return parseFloat(res.toFixed(2));
+}
+
+age('mercury', 2134835688);
+
