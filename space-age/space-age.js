fa875cd11781db66d32d0ac16b1e991dd83f0549
Elena <110896958+Elena-web@users.noreply.github.com>
2024-01-29 16:02:46 +0300

    add folder

---
+++ space-age/space-age.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+
+export const age = () => {
+  
+  return;
+};
