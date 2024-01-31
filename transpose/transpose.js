55ed56ab9cf5e09c88a29d42466f47d3f2c3d429
Glukozavrik <ploplotroll@gmail.com>
2024-01-31 14:32:53 +0300

    done

--- transpose/transpose.js
+++ transpose/transpose.js
@@ -3,6 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const transpose = () => {
-  throw new Error('Remove this statement and implement this function');
+export const transpose = (int) => {
+  if (int.length > 1) {
+    const newArr = [];
+    let secondArr = [];
+    let maxLength = 0;
+    int.forEach((ar) => { if (ar.length > maxLength) { maxLength = ar.length; } });
+    for (let i = 0; i < maxLength; i += 1) {
+      for (let j = int.length - 1; j >= 0; j -= 1) {
+        if (!(int[j][i] === undefined && secondArr.length === 0)) {
+          secondArr.unshift(int[j][i] ?? ' ');
+        }
+      }
+      newArr.push(secondArr.join(''));
+      secondArr = [];
+    }
+    return newArr;
+  } if (int.length === 1) {
+    return int[0].split('');
+  }
+  return int;
 };
+
